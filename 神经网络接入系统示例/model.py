3c1ffb79be9bf8a875ba88e615d15e406ad5a720
JKay Xiong <xiongjk15@gmail.com>
2023-12-16 00:01:26 +0800

    init

---
+++ 神经网络接入系统示例/model.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch
+
+class SimpleLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout_rate=0.2):
+        super(SimpleLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.bn = nn.BatchNorm1d(hidden_size)
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, _ = self.rnn(x, h0)
+        out = self.fc1(out[:, -1, :])
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.bn(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
